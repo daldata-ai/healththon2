81301362fd4e2d0a77986fa8cf68e010c48ad86c
Bilker1422 <58659211+Bilker1422@users.noreply.github.com>
2025-09-13 10:46:36 +0300

    fix image path

--- src/component/Timeline.tsx
+++ src/component/Timeline.tsx
@@ -4,9 +4,9 @@ export default function Timeline() {
     <div className="bg-inherit p-4 w-full">
       <picture>
         {/* Mobile first */}
-        <source srcSet="/timelineMoblie.svg" media="(max-width: 767px)" />
+        <source srcSet="timelineMoblie.svg" media="(max-width: 767px)" />
         {/* Desktop */}
-        <source srcSet="/timeline.svg" media="(min-width: 1028px)" />
+        <source srcSet="timeline.svg" media="(min-width: 1028px)" />
         {/* Fallback */}
         <img src="timeline.svg" alt="Timeline" className="w-full" />
       </picture>
