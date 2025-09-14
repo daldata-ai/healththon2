24a6a3aad8b40607883e9fb34684521adc64ad86
Bilker1422 <58659211+Bilker1422@users.noreply.github.com>
2025-09-14 18:23:03 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/",
+  base: "/healththon2/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
