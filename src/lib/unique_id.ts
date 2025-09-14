dcde926d161986ffd1d4754fd54db97287f941ad
Bilker1422 <58659211+Bilker1422@users.noreply.github.com>
2025-09-14 20:04:18 +0300

    full uuid

--- src/lib/unique_id.ts
+++ src/lib/unique_id.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export function getHumanReadableId() {
-  // Generate a uuid and take the first 8 characters for readability
-  return uuidv4().split('-')[0];
+  // Generate a full uuid for maximum uniqueness
+  return uuidv4();
 }
